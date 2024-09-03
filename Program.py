0ab07a57d6cb7011a4e56f15a96dd42a166b8946
Jakub Jaroš <jakub.jaros@certicon.cz>
2024-09-03 09:23:04 +0200

    Add project files.

---
+++ Program.py
@@ -0,0 +1,5 @@
+base_big = int(input("Enter a bigger base of a trapezoid"))
+base_small = int(input("Enter a  smaller base of a trapezoid"))
+height = int(input("Enter a height of a trapezoid"))
+area = (base_big + base_small) * height / 2
+print("Area is", area)
