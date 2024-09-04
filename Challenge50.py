30638fb75535f663bd97278285940d5e249c4cee
Jakub Jaroš <jakub.jaros@certicon.cz>
2024-09-04 13:54:11 +0200

    challenge 50

---
+++ Challenge50.py
@@ -0,0 +1,10 @@
+#---------------------------------------- Display multiplication table for a given number
+# num = int(input("Enter a number \n"))
+
+# count = 1
+# while count < 11:
+#     print(num, " x ", count, " = ", num * count)
+#     count +=1
+
+
+
--- Program.py
+++ Program.py
@@ -1,12 +1,6 @@
-year = int(input("Enter a year \n"))
+num = int(input("Enter a number \n"))
 
-y4 = year % 4 == 0
-y100 = year % 100 == 0
-y400 = year % 400 == 0
-
-if y400:
-    print ("This is a leap year")
-elif y4 and not y100:
-    print ("This is a leap year")
-else:
-    print ("This is not a leap year")
+count = 1
+while count < 11:
+    print(num, " x ", count, " = ", num * count)
+    count +=1
