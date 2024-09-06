e09c395146dbd882a822e95dec7ce5a81caab060
Jakub Jaroš <jakub.jaros@certicon.cz>
2024-09-06 11:03:21 +0200

    challenge 63

---
+++ Challenge63.py
@@ -0,0 +1,9 @@
+#---------------------------------------- Print prime numbers from 1 - 100
+# prime = 0
+# for number in range (1, 100+1):
+#     for i in range (1, number+1):
+#         if number % i == 0:
+#             prime += 1
+#     if prime <= 2:
+#         print(number)
+#     prime = 0
--- Program.py
+++ Program.py
@@ -1,12 +1,7 @@
-number = int(input("Enter a number \n"))
-
-prime = 0
-for i in range(2, number):
-    if number % i == 0:
-        prime += 1
-
-if prime == 0:
-    print(number, "is a prime number")
-else:
-    print(number, "is not a prime number")
-
+for i in range(0,5):
+    for j in range(0,5):
+        if i >= j:
+            print("*", end=" ")
+        else:
+            print("+", end = " ")
+    print("")
