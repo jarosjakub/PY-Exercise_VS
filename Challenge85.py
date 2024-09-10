4b6d93e983baa383a58b900a2e0d3aca006867f7
Jakub Jaroš <jakub.jaros@certicon.cz>
2024-09-10 15:26:05 +0200

    Challenge 85

---
+++ Challenge85.py
@@ -0,0 +1,33 @@
+#---------------------------------------- Check if two strings are anagram
+# s1 = input("Enter a string \n")
+# s2 = input("Enter another string \n")
+
+# l1 = len(s1)
+# l2 = len(s2)
+
+# if l1 -- l2:
+#     pass
+# else:
+#     print("different lenght of strings")
+
+# f = 0
+# anagram = True
+
+# for i in range(0, l2):
+#     if s2.find(s1[i]) >= 0:
+#         pass
+#     else:
+#         anagram = False
+
+# for i in range(0, l1):
+#     if s1.find(s2[i]) >= 0:
+#         pass
+#     else:
+#         anagram = False
+
+# if anagram:
+#      print ("The strings are anagram")
+# else:
+#     print ("The strings are not anagram")
+ 
+
--- Program.py
+++ Program.py
@@ -1,8 +1,30 @@
-date = input("Enter a date in dd/mm/yyyy format \n")
+s1 = input("Enter a string \n")
+s2 = input("Enter another string \n")
 
-sdate = date.split("/")
+l1 = len(s1)
+l2 = len(s2)
 
-print("Day:", sdate[0])
-print("Month:", sdate[1])
-print("Year:", sdate[2])
- 
+if l1 -- l2:
+    pass
+else:
+    print("different lenght of strings")
+
+f = 0
+anagram = True
+
+for i in range(0, l2):
+    if s2.find(s1[i]) >= 0:
+        pass
+    else:
+        anagram = False
+
+for i in range(0, l1):
+    if s1.find(s2[i]) >= 0:
+        pass
+    else:
+        anagram = False
+
+if anagram:
+     print ("The strings are anagram")
+else:
+    print ("The strings are not anagram")
