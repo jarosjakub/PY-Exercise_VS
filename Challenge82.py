321b9a8eb3f4615a02532ec968df4c19f7baad46
Jakub Jaroš <jakub.jaros@certicon.cz>
2024-09-10 12:02:31 +0200

    Challenge 82

--- Challenge82.py
+++ Challenge82.py
@@ -1,7 +1,9 @@
+#---------------------------------------- Find user id and domain name from email adress
 cnum = input("Enter your email \n")
 
 split = cnum.split("@")
 
 print("The user id is:", split[0])
 print("The domain name is:", split[1])
+
 
